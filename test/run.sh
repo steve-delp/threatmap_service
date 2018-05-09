be37e2fb3c89fbda9c976e3aa9a5f20019390ccb
steve <steve.delp@gmail.com>
2018-05-09 08:05:36 -0600

    Removed maven tasks from test script.

--- test/run.sh
+++ test/run.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 cd $(dirname $0)
 
-mvn clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-  exit $ret
-fi
-rm -rf target
-
-./gradlew build
+../gradlew build
 ret=$?
 if [ $ret -ne 0 ]; then
   exit $ret
